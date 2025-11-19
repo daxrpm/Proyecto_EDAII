96c9abcdbb797c3332db8c87d191aeeaf55f9dc7
Dax <daxnavarrete@gmail.com>
2025-11-19 09:46:05 -0500

    chore: remove unused aristas

--- grafo.py
+++ grafo.py
@@ -1,23 +1,15 @@
 from nodo import Nodo
+
+
 class Grafo:
 
-    #Constrcutor Grafo 
+    # Constrcutor Grafo
 
     def __init__(self):
         self.nodos = {}
-        self.aristas = []
 
     # Agregar nodo al grafo
     def agregar_nodo(self, nombre_nodo):
         if nombre_nodo not in self.nodos:
             nuevo_nodo = Nodo(nombre_nodo)
             self.nodos[nombre_nodo] = nuevo_nodo
-
-
-
-
-
-    # Agregar arista al grafo
-    #def agregar_arista(self, nodo_origen, nodo_destino, peso=1):
-
-    
